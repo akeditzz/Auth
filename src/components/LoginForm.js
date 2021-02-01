fa1d6e5671ca5d1b5addd9c294a0feb4f44d022b
akeditzz <akshaymanagooli@yahoo.com>
2021-02-01 18:04:52 +0530

    Login form with reusable InputText done

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,37 +1,33 @@
 import React, { Component } from 'react'
-import { StyleSheet, TextInput } from 'react-native'
-import { Button, Card, CardSection } from './common'
+import { Button, Card, CardSection, Field } from './common'
 
 class LoginForm extends Component {
-    state = { text: '' }
+    state = { email: '' ,password:''}
 
     render() {
         return <Card>
             <CardSection >
-                <TextInput
-                    value={this.state.text}
-                    style={styles.textInputStyle}
-                    onChangeText={text => this.setState({ text })}
+                <Field
+                    label="Email"
+                    value={this.state.email}
+                    onChangeText={email => this.setState({ email })}
+                    placeholder="user@gmail.com"
                 />
             </CardSection>
-            <CardSection />
+            <CardSection >
+                <Field
+                    label="Password"
+                    value={this.state.password}
+                    onChangeText={password => this.setState({ password })}
+                    placeholder="password"
+                    secureTextEntry
+                />
+            </CardSection>
             <CardSection >
                 <Button title="Log In" />
             </CardSection>
         </Card>
     }
 }
-
-const styles = StyleSheet.create({
-    textInputStyle: {
-        height: 40,
-        width: 0,
-        flex: 1,
-        flexDirection: 'row',
-        borderColor: '#000',
-        borderWidth: 1,
-        borderRadius: 4
-    }
-})
 
 export default LoginForm
